0604a899b8d32909cc2c4785e1966facbb573335
Sachindhabale9146 <161546105+Sachindhabale9146@users.noreply.github.com>
2025-01-02 11:04:44 +0530

    Create Addition_Two_Number.java
    addition of two number

---
+++ Addition_Two_Number.java
@@ -0,0 +1,12 @@
+package newpackage;
+
+public class Addition_Two_Number {
+
+	public static void main(String[] args) {
+		
+		int a=9, b =5;
+		
+		a = a+b;
+		System.out.println("addtion of two number : "+a);
+	}
+}
